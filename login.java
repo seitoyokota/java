505581cef86c5ffc9ea19ad3f7ac7a2b14764369
Seito Yokota <seitoyokota15@icloud.com>
2026-03-23 09:10:41 -0500

    check

--- login.java
+++ login.java
@@ -16,25 +16,29 @@ public class login {
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("1. create account\n2. login\nEnter your choice: ");
+        System.out.print("1. create account\n2. login\n3. Exit\nEnter your choice: ");
         int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.print("Enter username: ");
-                String user = scanner.next();
-                System.out.print("Enter password: ");
-                String pass = scanner.next();
-                createAccount(user, pass);
-                break;
-            case 2:
-                System.out.print("Enter username: ");
-                user = scanner.next();
-                System.out.print("Enter password: ");
-                pass = scanner.next();
-                loginAccount(user, pass);
-                break;
-            default:
-                System.out.println("Invalid choice.");
+        while(choice != 3) {
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter username: ");
+                    String user = scanner.next();
+                    System.out.print("Enter password: ");
+                    String pass = scanner.next();
+                    createAccount(user, pass);
+                    break;
+                case 2:
+                    System.out.print("Enter username: ");
+                    user = scanner.next();
+                    System.out.print("Enter password: ");
+                    pass = scanner.next();
+                    loginAccount(user, pass);
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+            System.out.print("1. create account\n2. login\n3. Exit\nEnter your choice: ");
+            choice = scanner.nextInt();
         }
     }
 }
